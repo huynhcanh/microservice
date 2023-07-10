5e4208873b3b977279729ad626106f27257470ce
Your Name <you@example.com>
2023-07-10 22:47:39 +0700

    add userservice

---
+++ userservice/src/main/java/com/huynhcanh/userservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.huynhcanh.userservice.controller;
+
+import com.huynhcanh.userservice.data.User;
+import com.huynhcanh.userservice.model.UserDTO;
+import com.huynhcanh.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/listUser")
+    public List<User> getAllUser() {
+        return userService.getAllUser();
+    }
+    @PostMapping("/addUser")
+    public UserDTO addUser(@RequestBody UserDTO dto) {
+        return userService.saveUser(dto);
+    }
+    @PostMapping("/login")
+    public UserDTO login(@RequestBody UserDTO dto) {
+        return userService.login(dto.getUsername(), dto.getPassword());
+    }
+}
