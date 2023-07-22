020307bd226efdcd99f307d60533ad8ce2f7b6d0
Your Name <you@example.com>
2023-07-23 00:54:58 +0700

    add cau chi for Notificationservice

--- notificationservice/src/main/java/com/huynhcanh/notificationservice/NotificationserviceApplication.java
+++ notificationservice/src/main/java/com/huynhcanh/notificationservice/NotificationserviceApplication.java
@@ -18,9 +18,34 @@ public class NotificationserviceApplication {
 
 	private Logger logger =org.slf4j.LoggerFactory.getLogger(NotificationserviceApplication.class);
 
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+
+	@Autowired
+	private CircuitBreakerFactory circuitBreakerFactory;
+
 	@StreamListener(Sink.INPUT)
 	public void consumeMessage(Message message) {
-		logger.info("Consume Payload: "+message.getEmployeeId()+" "+message.getMessage());
+
+		EmployeeReponseModel Employeemodel = circuitBreakerFactory.create("getEmployee").run(
+				() -> { EmployeeReponseModel model = webClientBuilder.build()
+						.get()
+						.uri("http://localhost:9002/api/v1/employees/"+message.getEmployeeId())
+						.retrieve()
+						.bodyToMono(EmployeeReponseModel.class)
+						.block();
+					return model;
+				},
+				t -> { EmployeeReponseModel model = new EmployeeReponseModel();
+					model.setFirstName("Anonymous");
+					model.setLastName("Employee");
+					return model;
+				}
+		);
+
+		if(Employeemodel !=null) {
+			logger.info("Consume Payload: "+Employeemodel.getFirstName()+ " "+Employeemodel.getLastName()+" "+message.getMessage());
+		}
 	}
 
 	public static void main(String[] args) {
--- notificationservice/src/main/java/com/huynhcanh/notificationservice/configuration/AppConfiguration.java
+++ notificationservice/src/main/java/com/huynhcanh/notificationservice/configuration/AppConfiguration.java
@@ -11,25 +11,25 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
 
-//@Configuration
+@Configuration
 public class AppConfiguration {
-//
-//    @Bean
-//    public WebClient.Builder getWebClientBuilder(){
-//        return WebClient.builder();
-//    }
-//
-//    @Bean
-//    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
-//        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-//                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build())
-//                .circuitBreakerConfig(CircuitBreakerConfig.custom()
-//                        .slidingWindowSize(10)
-//                        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
-//                        .minimumNumberOfCalls(5)
-//                        .failureRateThreshold(50)
-//                        .build()
-//                ).build()
-//        );
-//    }
+
+    @Bean
+    public WebClient.Builder getWebClientBuilder(){ // call REST
+        return WebClient.builder();
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build())
+                .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .slidingWindowSize(10)
+                        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
+                        .minimumNumberOfCalls(5)
+                        .failureRateThreshold(50)
+                        .build()
+                ).build()
+        );
+    }
 }
